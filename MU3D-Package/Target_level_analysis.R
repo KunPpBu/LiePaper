a4c936e70b6aa67d0f906841d4137fcb957bdac5
amanda <kunbu@mail.usf.edu>
2022-08-03 00:08:16 -0400

    Updated correlation plot

--- MU3D-Package/Target_level_analysis.R
+++ MU3D-Package/Target_level_analysis.R
@@ -1,6 +1,9 @@
+library(corrplot)
 #import target level dataset
 MU3D_Target_Level_Data0 <- read.csv("MU3D_Target_Level_Data.csv")
-
+#remove veractiy first
+MU3D_Video_Level_Data <- MU3D_Video_Level_Data0[,-3]
+str(MU3D_Video_Level_Data)
 
 pie(table(MU3D_Target_Level_Data0$Age),radius = 1, cex = 0.4)
 library(dplyr)
@@ -13,3 +16,6 @@ ggplot(dataset, aes(x = "", y = Count, fill = Age)) +
   geom_col(width = 1) + 
   coord_polar(theta = "y") +
   xlab(NULL)
+
+p.cor = cor(MU3D_Video_Level_Data[,-2])
+corrplot.mixed(p.cor, title(main = "Correlation plot for features"),mar=c(0,0,1,0))
